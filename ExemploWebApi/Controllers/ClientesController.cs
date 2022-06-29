9b9ff30c2809626b69093becffc0b37a78855185
Jefferson <jefferson.amsantos@hotmail.com>
2022-06-29 12:03:13 -0300

    concluido

---
+++ ExemploWebApi/Controllers/ClientesController.cs
@@ -0,0 +1,33 @@
+﻿using ExemploWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ExemploWebApi.Controllers
+{
+    public class ClientesController : ApiController
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        public List<Cliente> Get() /*vai retorna uma lista de cliente*/
+        {
+            return clientes; /*vai retornar os itens que tem dentro da lista a cima*/
+        }
+
+        public void Post(string nome)
+        {
+            if(!string.IsNullOrEmpty(nome))//se nao for vazio e null vai entrar na lista
+            clientes.Add(new Cliente(nome));
+        }
+
+        public void Delete(string nome)
+        {
+            clientes.RemoveAt(clientes.IndexOf(clientes.First(x => x.Nome.Equals(nome))));
+        }
+
+
+    }
+}
---
+++ Readme.md.txt
@@ -0,0 +1,7 @@
+Projeto simples de web api com os seguintes metodos.
+
+List - para retornar uma lista de clientes
+
+Post - para adicionar cliente na lista
+
+Delete - para deletar algum cliente
